9041444a584cf4f0bae4f4d1fc7d927764d43d12
Ori Erez <orierez3@gmail.com>
2026-06-09 11:18:10 -0400

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,15 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        sorts = sorted(intervals, key=lambda x: x.start)      
+        for i in range(len(sorts)-1):
+            if sorts[i].end > sorts[i+1].start:
+                return False
+        return True
