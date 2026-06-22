e859bf7beb818aaeb3df6ef2c3ad4c43e0887808
Ori Erez <orierez3@gmail.com>
2026-06-22 14:28:17 -0400

    Add: is-palindrome - submission-22

---
+++ Data Structures & Algorithms/is-palindrome/submission-22.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.size()-1;
+        while (l<r){
+            while (!isalnum(s[l]) && l<r){
+                l++;
+            }
+            while (!isalnum(s[r]) && l<r){
+                r--;
+            }
+            if (tolower(s[l]) == tolower(s[r])){
+                l++;
+                r--;
+            }
+            else{
+                return false;
+            }
+
+        }
+        return true;
+    }
+};
